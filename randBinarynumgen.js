6be50f7bccf02a4f54060d01bee2962ee8c00bb4
Albert Lipaev <albert.lipaev@wsu.edu>
2023-02-14 18:24:16 -0500

    Added support for 12 bit numbers

--- randBinarynumgen.js
+++ randBinarynumgen.js
@@ -1,14 +1,15 @@
 var randomBinary = require('random-binary');
 const { performance } = require('perf_hooks');
+const { systemPreferences } = require('electron');
 // randomBinary(4); or 8 or 16 bits to generate different binary number 
 // with different number of bits
 
-mode = true; // true for continuous, false for "run once" mode
+mode = false; // true for continuous, false for "run once" mode
 // need to config how to tie it to the button
 
 var array = [];
 for (let j = 0; j < 100; j++) { // for loop to populate the array to simulate the data block
-    array.push(parseInt(randomBinary(4), 2));
+    array.push(parseInt(randomBinary(12), 2)); // 12 bit binary value
 }
     
 i = 0;
@@ -18,8 +19,11 @@ while (i < 100) { // will be replace with data listener module
         console.log(parseInt(randomBinary(4), 2));  
         i++;
     } else if (mode == false) {
-        console.log(array.pop())
+        console.log(array.pop());
+        if (!array.length) {
+            process.exit()
+        }
     }
 }
-var endTime = performance.now()
-console.log(`took ${endTime - startTime} milliseconds`)
+var endTime = performance.now();
+console.log(`took ${endTime - startTime} milliseconds`);
