314436e264480d8665e98e84523fa805d7a0e1d5
Albert Lipaev <albert.lipaev@wsu.edu>
2023-02-14 14:42:21 -0500

    Added func to measure speed of randnum generator

--- randBinarynumgen.js
+++ randBinarynumgen.js
@@ -1,10 +1,15 @@
 var randomBinary = require('random-binary');
+const { performance } = require('perf_hooks');
 // randomBinary(4); or 8 or 16 bits to generate different binary number 
 // with different number of bits
 i = 0;
+
+var startTime = performance.now()
 while (i<10) {
     // generates 4 bit numbers
     // parseInt(x, 2) converts any binary number to decimal 
     console.log(parseInt(randomBinary(4), 2));  
     i++;
 }
+var endTime = performance.now()
+console.log(`took ${endTime - startTime} milliseconds`)
