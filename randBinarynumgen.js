3d2740c764209c5bff072689e680fdcaea5cf5e9
Albert Lipaev <albert.lipaev@wsu.edu>
2023-02-14 14:38:30 -0500

    added decimal converter

--- randBinarynumgen.js
+++ randBinarynumgen.js
@@ -1,7 +1,10 @@
 var randomBinary = require('random-binary');
-
+// randomBinary(4); or 8 or 16 bits to generate different binary number 
+// with different number of bits
 i = 0;
-while (i<100) {
-    console.log(randomBinary());
+while (i<10) {
+    // generates 4 bit numbers
+    // parseInt(x, 2) converts any binary number to decimal 
+    console.log(parseInt(randomBinary(4), 2));  
     i++;
 }
