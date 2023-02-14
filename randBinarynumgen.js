dbc3ad3e65334b9162e8284e6c2f7a12b51961d3
Albert Lipaev <albert.lipaev@wsu.edu>
2023-02-14 17:42:10 -0500

    added support for cont/single run mods

--- randBinarynumgen.js
+++ randBinarynumgen.js
@@ -2,14 +2,24 @@ var randomBinary = require('random-binary');
 const { performance } = require('perf_hooks');
 // randomBinary(4); or 8 or 16 bits to generate different binary number 
 // with different number of bits
-i = 0;
 
+mode = true; // true for continuous, false for "run once" mode
+// need to config how to tie it to the button
+
+var array = [];
+for (let j = 0; j < 100; j++) { // for loop to populate the array to simulate the data block
+    array.push(parseInt(randomBinary(4), 2));
+}
+    
+i = 0;
 var startTime = performance.now()
-while (i<10) {
-    // generates 4 bit numbers
-    // parseInt(x, 2) converts any binary number to decimal 
-    console.log(parseInt(randomBinary(4), 2));  
-    i++;
+while (i < 100) { // will be replace with data listener module
+    if (mode == true) {
+        console.log(parseInt(randomBinary(4), 2));  
+        i++;
+    } else if (mode == false) {
+        console.log(array.pop())
+    }
 }
 var endTime = performance.now()
 console.log(`took ${endTime - startTime} milliseconds`)
